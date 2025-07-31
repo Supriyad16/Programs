eb8353c03d88d2d592b91a027da13c5f13a8eb2f
Supriya D <dinakaranctsupriya@gmail.com>
2025-07-31 10:56:19 +0530

    Concatenation of two numbers

---
+++ codes/src/com/xworkz/codes/Concate.java
@@ -0,0 +1,13 @@
+package com.xworkz.codes;
+
+public class Concate {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        System.out.print(a);
+        System.out.print(b);
+
+    }
+}
