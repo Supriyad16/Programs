8806236409bf256df3e70887bdfeb024b1e6fe9b
Supriya D <dinakaranctsupriya@gmail.com>
2025-07-31 11:11:20 +0530

    Conversion of one data type to another

---
+++ codes/src/com/xworkz/codes/TypeConversion.java
@@ -0,0 +1,13 @@
+package com.xworkz.codes;
+
+public class TypeConversion {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        double d = (int) a;
+
+        System.out.println(a);
+        System.out.println(d);
+    }
+}
