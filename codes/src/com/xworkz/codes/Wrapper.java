1d5c18cb35f3b650495dd444bb31ec8ab880e831
Supriya D <dinakaranctsupriya@gmail.com>
2025-07-31 11:13:12 +0530

    Usage of wrapper class

---
+++ codes/src/com/xworkz/codes/Wrapper.java
@@ -0,0 +1,13 @@
+package com.xworkz.codes;
+
+public class Wrapper {
+
+    public static void main(String[] args) {
+
+        String usn = "54";
+        int num = Integer.parseInt(usn);
+
+        System.out.println(usn);
+        System.out.println(num);
+    }
+}
